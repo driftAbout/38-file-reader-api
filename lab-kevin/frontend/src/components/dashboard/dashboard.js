0943e25f72a48a8f067e2e68ccd0b46612a019cf
Kevin Miller <kevin@driftabout.com>
2018-03-09 22:44:26 -0800

    flyout working

--- lab-kevin/frontend/src/components/dashboard/dashboard.js
+++ lab-kevin/frontend/src/components/dashboard/dashboard.js
@@ -1,23 +1,40 @@
+import './_dashboard.scss';
 import React from 'react';
 import {Settings} from '../settings';
 import {connect} from 'react-redux';
 
-class Dashboard extends React.Component{
 
+class Dashboard extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      isClosed: true,
+    };
+
+    this.toggleSettings = this.toggleSettings.bind(this);
+  }
   // componentWillReceiveProps(nextProps){
   //   this.props.createProfileRequest(nextProps.token);
   // }
 
+  toggleSettings(){
+    this.setState({isClosed: !this.state.isClosed});
+  }
+
   render(){
     return (
-      <section className="dashboard-container">
+      <section className="dashboard">
         <h2>All I&apos;ve got is a photograph<span>--Def Leppard</span></h2>
         <p>Take a picture here, Take a souvenir<span>-—R.E.M.</span></p>
-        <Settings />
+        <div className={`settings-wrap${this.state.isClosed ? ' closed' : ''}`}>  
+          <span className="settings-toggle" onClick={this.toggleSettings}>{this.state.isClosed ? 'Settings' : 'close'}</span>
+          <Settings />
+        </div>
+        <div className="gallery-wrap" >
+        </div>
       </section>
     );
   }
-
 
 } 
 
